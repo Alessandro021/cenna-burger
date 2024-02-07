a809750087c7300a124cdbdbaa73410bcec349cf
alessandro <alessandrodamata6@gmail.com>
2024-02-07 14:11:12 -0300

    feat: criado o componente de header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/components/header";
 import {View, Text} from "react-native"
 
 const Home = () => {
 return(
     <View className="bg-slate-900 flex-1">
-        <Text className="text-white">Ola mundo</Text>
+       <Header cartQuantityItems={1000} title="Faça seu pedido" />
     </View>
 )
 }
