32e41380ae9a6bea7f048389c03c2f4a88f44b23
alessandro <alessandrodamata6@gmail.com>
2024-02-06 21:39:16 -0300

    feat: add fonte ao projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/app/**/*.{ts,tsx}",  "./src/components/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: "Inter_600SemiBold",
+        subtitle: "Inter_500Medium",
+        body: "Inter_400Regular",
+        bold: "Inter_700Bold",
+      }
+    },
   },
   plugins: [],
 }
