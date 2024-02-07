341fe7af2efa6ae9d589cd8045377ac87d00fda0
alessandro <alessandrodamata6@gmail.com>
2024-02-06 21:11:17 -0300

    feat: add tailwind ao projeto

---
+++ README.md
@@ -0,0 +1,5 @@
+###Instalar nativeWind
+- npm i nativewind@2.0.11    -> baixar essa versao
+
+###Instalar tailwindcss
+- npm i tailwindcss@3.3.2 --save-dev
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import {View, Text} from "react-native"
 
 const Home = () => {
 return(
-    <View>
-        <Text>Ola mundo</Text>
+    <View className="bg-slate-900 flex-1">
+        <Text className="text-white">Ola mundo</Text>
     </View>
 )
 }
