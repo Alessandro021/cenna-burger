910434e1dd2e8ada8aa66f8aebd90704a63a2213
alessandro <alessandrodamata6@gmail.com>
2024-02-07 15:11:16 -0300

    feat: criado o componente de category-button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,37 @@
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import {View, Text} from "react-native"
+import {View, Text, FlatList} from "react-native"
+import {CATEGORIES} from "@/utils/data/products"
+import { useCallback, useState } from "react";
+
+interface ItemProps {
+    item: string
+}
 
 const Home = () => {
+
+  const [ category , setCategory ] = useState<string>(CATEGORIES[0])
+
+  const renderItem = useCallback(({item}: ItemProps) => (
+    <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)} />
+  ),[category])
+
+  const handleCategorySelect = (selectedCategory: string) => {
+    setCategory(selectedCategory)
+  }
 return(
     <View className="bg-slate-900 flex-1">
        <Header cartQuantityItems={1000} title="Faça seu pedido" />
+      
+    <FlatList
+    className="max-h-10 mt-5"
+        data={CATEGORIES}
+        keyExtractor={item => item}
+        renderItem={renderItem}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+    />
     </View>
 )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,7 @@ export const Header = ({title, cartQuantityItems = 0}: HeaderProps) => {
             {cartQuantityItems > 0 && (
                 <TouchableOpacity className="relative" activeOpacity={0.7}>
                     <View className="bg-lime-300 w-4 h-4 rounded-full items-center justify-center top-2 z-10 -right-3.5">
-                        <Text className="text-slate-900 font-bold text-xs">{cartQuantityItems}</Text>
+                        <Text className="text-slate-900 font-bold text-xs">{cartQuantityItems > 99 ? "99+" : cartQuantityItems}</Text>
                     </View>
 
                     <Feather name="shopping-bag" color={colors.white} size={24} />
