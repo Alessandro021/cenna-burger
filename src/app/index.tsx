bc16f708e75910d2b7812fe6f23370410a987545
alessandro <alessandrodamata6@gmail.com>
2024-02-13 17:26:23 -0300

    feat: criado o componente de produto e a ref do tipo de produto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,9 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import {View, Text, FlatList} from "react-native"
-import {CATEGORIES} from "@/utils/data/products"
-import { useCallback, useState } from "react";
+import {View, Text, FlatList, SectionList} from "react-native"
+import {CATEGORIES, MENU} from "@/utils/data/products"
+import { useCallback, useState, useRef } from "react";
+import { Product, ProductDataProps } from "@/components/product";
 
 interface ItemProps {
     item: string
@@ -11,13 +12,27 @@ interface ItemProps {
 const Home = () => {
 
   const [ category , setCategory ] = useState<string>(CATEGORIES[0])
+  const sectionListRef = useRef<SectionList>(null)
 
   const renderItem = useCallback(({item}: ItemProps) => (
     <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)} />
   ),[category])
 
+  const renderItemSectionList = useCallback(({item}: any) => (
+    <Product data={item} />
+  ),[])
+
   const handleCategorySelect = (selectedCategory: string) => {
     setCategory(selectedCategory)
+    const sectionIndex = CATEGORIES.findIndex(category => category === selectedCategory)
+
+    if(sectionListRef.current){
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex: sectionIndex,
+        itemIndex: 0
+      })
+    }
   }
 return(
     <View className="bg-slate-900 flex-1">
@@ -31,6 +46,18 @@ return(
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{gap: 12, paddingHorizontal: 20}}
+    />
+
+    <SectionList
+      className="flex-1 p-5"
+      ref={sectionListRef}
+      sections={MENU}
+      keyExtractor={item => item.id}
+      stickySectionHeadersEnabled={false}
+      renderItem={renderItemSectionList}
+      renderSectionHeader={({section : {title}}) => <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>}
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{paddingBottom: 100}}
     />
     </View>
 )
---
+++ src/components/product.tsx
@@ -0,0 +1,27 @@
+import { Image, Text, ImageProps, TouchableOpacity, TouchableOpacityProps, View } from "react-native"
+
+export interface ProductDataProps {
+    title: string
+    description: string
+    thumbnail: ImageProps
+}
+
+type ProductProps = TouchableOpacityProps & {
+    data: ProductDataProps
+}
+
+export const Product = ({data, ...rest}: ProductProps) => {
+    return(
+        <TouchableOpacity className=" w-full flex-row items-center pb-4" {...rest}>
+            <Image
+            className="w-20 h20 rounded-md"
+            source={data.thumbnail}
+            />
+
+            <View className="flex-1 ml-3">
+                <Text className="text-slate-100 font-subtitle text-base flex-1">{data.title}</Text>
+                <Text className="text-slate-400 text-xs leading-5 mt-0.5">{data.description}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
