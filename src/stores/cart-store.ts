5ece1b69b0cf127ca36dc47d0280567e94f75b73
alessandro <alessandrodamata6@gmail.com>
2024-02-14 17:52:24 -0300

    feat: persistindo os dados com o zustend e o asyncstorage

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,19 +1,21 @@
 import { ProductProps } from "@/utils/data/products"
 import { create } from "zustand"
 import * as cartInMemory from "./helpers/cart-in-memory"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import {createJSONStorage, persist} from "zustand/middleware"
 
 
 export type ProductCartProps = ProductProps & {
     quantity: number
 }
 
-interface StateProps {
+interface StateProps { 
     products: ProductCartProps[]
     add: (product: ProductProps) => void
     remove: (productId: string) => void
 }
 
-export const useCartStore = create<StateProps>(set => ({
+export const useCartStore = create(persist<StateProps>(set => ({
     products: [],
     add: (product: ProductProps) => set(state => ({
         products: cartInMemory.add(state.products, product)
@@ -21,4 +23,7 @@ export const useCartStore = create<StateProps>(set => ({
     remove: (productId: string) =>  set(state => ({
         products: cartInMemory.remove(state.products, productId)
     }))
+}), {
+    name: "cenna-burger:cart",
+    storage: createJSONStorage(() => AsyncStorage)
 }))
